31c631e28b526b6af444705d6c5ab7cd553b39c7
Shreya Jain <57009626+shreyaajainn31@users.noreply.github.com>
2022-02-09 12:43:26 -0500

    Create java-int-to-string.java

---
+++ Java/Introduction/java-int-to-string.java
@@ -0,0 +1,34 @@
+/*
+You are given an integer , you have to convert it into a string.
+
+Please complete the partially completed code in the editor. If your code successfully converts  into a string  the code will print "Good job". Otherwise it will print "Wrong answer".
+
+n can range between -100 to 100 inclusive.
+
+Sample Input 0
+100
+Sample Output 0
+Good job
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+       
+        String s = Integer.toString(n);
+        
+        if(n == Integer.parseInt(s)){
+            System.out.println("Good job");
+        }
+        else{
+            System.out.println("Wrong answer");
+        }
+        
+    }
+}
